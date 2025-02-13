6cbbd52fbb5f5058c22dd9b639c660f073052f18
SoleneOclock <solene@oclock.io>
2025-02-13 09:27:14 +0100

    install de biome à la place de eslint et du coup biome n'aime pas les non null assertion donc au lieu de desactiver la regle on change le code du main.tsx et on ajoute un if pour verifier que le div root existe bien

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// getElementById renvoie une valeur de type HTMLElement ou null , nous on met le ! qui dit c'est pas null donc c'est forcement un Element
+// createRoot accepte en paramètre un container (Element ou DocumentFragment ou Document)
+
+const rootElt = document.getElementById('root');
+
+if (rootElt) {
+  createRoot(rootElt).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+} else {
+  console.log("erreur il n'y a pas de div root dans la page");
+}
